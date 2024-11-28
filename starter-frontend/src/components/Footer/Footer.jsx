a7f9a03b653d6abed9be2322e7f58d9590f0422a
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-28 12:55:55 -0800

    Update Footer

--- starter-frontend/src/components/Footer/Footer.jsx
+++ starter-frontend/src/components/Footer/Footer.jsx
@@ -9,14 +9,15 @@ import {
   Select,
   Box,
   IconButton,
+  ButtonGroup
 } from "@mui/material";
 
 import { Twitter, Facebook, YouTube, Pinterest, RssFeed } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#333" }}>
-      <Toolbar >
+    <AppBar position="static" >
+      <Toolbar sx={{ backgroundColor: "#333" }} > 
 
         <Box sx={{ display: "flex", alignItems: "center", gap: 4, pl: '10vw' }}>
           <Button color="inherit" href="#forum" sx={{ whiteSpace: 'nowrap', gap: '7px', fontSize: '12px' }} ><img src="assets/img/dashboard/footer/optimize.png" />TV 16:9 Optimized</Button>
@@ -43,6 +44,48 @@ const Footer = () => {
               <RssFeed />
             </IconButton>
           </Box>
+        </Box>
+
+      </Toolbar>
+
+      <Toolbar sx={{ backgroundColor: "#fff", }} >
+
+        <Box sx={{ display: "flex", alignItems: "center", gap: 4,  }}>
+            <img src="assets/img/dashboard/footer/google_webstore.jpg" height="50px" />
+        </Box>
+        <Box sx={{ display: 'block', }}>
+          <Box sx={{ display: "flex", gap: 1, pl: 10, }}>
+            <ButtonGroup variant="text" aria-label="Basic button group">
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Terms of use</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Privacy Policy</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Getting Started</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Guidelines</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>FAQ</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Facebook</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Work with us</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Dealers</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Buys and ad</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Developers</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Brands</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>About</Button>
+            </ButtonGroup>
+          </Box>
+          <Box sx={{ display: "flex", gap: 1, pl: 10, }}>
+            <ButtonGroup variant="text" aria-label="Basic button group">
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Italiano</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>English(US)</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Expanol</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Portuguise(Brasil)</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Francaise(France)</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Deutsch</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Arabic</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Hindi</Button>
+                <Button sx={{ whiteSpace: 'nowrap', fontSize: '10px', color: '#333' }}>Chinese</Button>
+            </ButtonGroup>
+          </Box>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 4, pl: '5vw', width: '15%' }}>
+            <Button sx={{ whiteSpace: 'nowrap', fontSize: '12px', color: '#333' }}>Downloads</Button>
         </Box>
 
       </Toolbar>
