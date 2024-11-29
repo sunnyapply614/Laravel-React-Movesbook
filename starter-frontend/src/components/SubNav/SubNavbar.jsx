8cf3c0487cb4da5dd282125706f4af88258ddfc3
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-29 02:07:55 -0800

    Update Header

--- starter-frontend/src/components/SubNav/SubNavbar.jsx
+++ starter-frontend/src/components/SubNav/SubNavbar.jsx
@@ -8,10 +8,18 @@ import {
   Tabs, 
   Tab,
 } from "@mui/material";
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Divider from '@mui/material/Divider';
 
 const SubNavbar = () => {
 
+  const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState(0);
+
+  const toggleSection = () => {
+      setIsOpen(!isOpen);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -19,14 +27,12 @@ const SubNavbar = () => {
 
   return (
     <AppBar position="static" sx={{ backgroundColor: "#E8E8E8", color: "#AF180F",  pl: '30px'  }}>
-      <Toolbar sx={{ justifyContent: "space-between", gap: '10px' }}>
+      <Toolbar sx={{ gap: '10px' }}>
         {/* Left Section */}
         <Box sx={{ display: "flex", gap: 3, backgroundColor: "#E5C11A", width: "30vw", }}>
           <Button color="inherit" href="#why-movesbook">Profiles: See who's using Movesbook</Button>
         </Box>
-        
         <img src="assets/img/dashboard/ajax-loader(1)90.gif" />
-
         <Tabs
             value={value}
             onChange={handleChange}
@@ -34,7 +40,7 @@ const SubNavbar = () => {
             sx={{
                 display: 'flex',  // Ensures the tabs are displayed as a row
                 flexDirection: 'column',  // Keeps tabs in a horizontal layout
-                width: '100%'  // Ensures it takes full width
+                width: '85%'  // Ensures it takes full width
               }}
         >
             <Tab value={0} label="VIP users" />
@@ -47,8 +53,20 @@ const SubNavbar = () => {
             <Tab value={7} label="last logged" />
             <Tab value={8} label="our clubs" />
         </Tabs>
-
+        <Box sx={{ margin: '0px', cursor: 'pointer', width: '7%' }} onClick={toggleSection}>
+          {isOpen ? (
+                  <ExpandLessIcon sx={{ color: '#333' }} />
+              ) : (
+                  <ExpandMoreIcon sx={{ color: '#333' }} />
+              )}
+        </Box>
       </Toolbar>
+      <Divider variant="middle"  />
+      <Box>
+        {isOpen && (
+            <Box sx={{ height: '200px' }} />
+        )}
+      </Box>
     </AppBar>
   );
 };
