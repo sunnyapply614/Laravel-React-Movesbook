4af3c61a4bf5c05107920c88151ec3d92867ddb6
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-29 16:55:50 -0800

    Update Footer.jsx

--- starter-frontend/src/components/Footer/Footer.jsx
+++ starter-frontend/src/components/Footer/Footer.jsx
@@ -26,25 +26,7 @@ const Footer = () => {
           <Button color="inherit" href="#blog" sx={{ whiteSpace: 'nowrap', gap: '7px', fontSize: '12px' }} ><img src="assets/img/dashboard/footer/mobile_version.png" />Mobile Version</Button>
           <Button color="inherit" href="#blog" sx={{ whiteSpace: 'nowrap', gap: '7px', fontSize: '12px' }} ><img src="assets/img/dashboard/footer/cloud_storage.png" />Cloud Storage</Button>
         </Box>
-        <Box>
-          <Box sx={{ display: "flex", gap: 1, pl: 10, }}>
-            <IconButton href="#twitter" color="inherit" aria-label="Twitter">
-              <Twitter />
-            </IconButton>
-            <IconButton href="#facebook" color="inherit" aria-label="Facebook">
-              <Facebook />
-            </IconButton>
-            <IconButton href="#youtube" color="inherit" aria-label="YouTube">
-              <YouTube />
-            </IconButton>
-            <IconButton href="#pinterest" color="inherit" aria-label="Pinterest">
-              <Pinterest />
-            </IconButton>
-            <IconButton href="#rss" color="inherit" aria-label="RSS">
-              <RssFeed />
-            </IconButton>
-          </Box>
-        </Box>
+
 
       </Toolbar>
 
