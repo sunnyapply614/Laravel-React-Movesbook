9f8403cc2fa25d22b95ac780d04c16d4d0fd0845
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-29 16:54:26 -0800

    Update DashboardPage.jsx

--- starter-frontend/src/components/Dashboard/DashboardPage.jsx
+++ starter-frontend/src/components/Dashboard/DashboardPage.jsx
@@ -1,4 +1,12 @@
-
+import React from "react";
+import HeaderTab from "../HeaderTab/HeaderTab";
+import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
+import HeroSlider from "../Slider/HeroSlider";
+import SubNavbar from "../SubNav/SubNavbar";
+import Maindata from "../Maindata/Maindata";
+import Footer from "../Footer/Footer";
+import Sidetab from "../Side/Sidetab";
 
 console.log('dash');
 
